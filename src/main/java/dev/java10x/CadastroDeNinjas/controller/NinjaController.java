ea9fcb1d12fc85336038eeabd76f7a827cd65912
Pedro Santos <pedrohssantos1509@gmail.com>
2026-03-26 14:35:32 -0300

    feat: add new Entity(Ninja)

--- src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
@@ -1,4 +1,4 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ninja")
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/model/NinjaModel.java
@@ -0,0 +1,72 @@
+package dev.java10x.CadastroDeNinjas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "idade")
+    private int idade;
+
+    @Column(name = "email")
+    private String email;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Ninja{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
