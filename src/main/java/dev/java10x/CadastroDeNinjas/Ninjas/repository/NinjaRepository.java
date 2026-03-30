f3deb32252c3790a3114c62a25815e0981880f89
Pedro Santos <pedrohssantos1509@gmail.com>
2026-03-30 18:56:44 -0300

    feat: add Repository and Service classes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/repository/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.Ninjas.repository;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.model.NinjaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
