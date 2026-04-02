e2e9e64b07b310e176be16faa523465be6268a04
Pedro Santos <pedrohssantos1509@gmail.com>
2026-04-02 15:46:38 -0300

    feat: update NinjaController endpoints

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/controller/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninja")
@@ -12,5 +10,33 @@ public class NinjaController {
     public String boasVindas(){
         return "Bem Vindo ao cadastro de ninjas";
     }
+
+    //Adicionar ninja (POST) (CREATE)
+    @PostMapping("/criar")
+    public String adicionarNinja(){
+        return "Ninja criado";
+    }
+
+    //findById (GET) (READ)
+    @GetMapping("/{id}")
+    public String buscaNinjaPorId(@PathVariable Long id){
+        return "Ninja encontrado neste id: ";
+    }
+    //Mostrar todos os ninjas (GET) (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Todos os ninjas";
+    }
+    //Atualizar Ninja (PUT) (UPDATE)
+    @PutMapping("/atualizar")
+    public String atualizarNinja(){
+        return "Ninja Atualziado";
+    }
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja deletado";
+    }
+
 
 }
