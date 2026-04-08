407a507d0b182e9e018706b78b06af3595971d03
pedro-santos15 <pedrohssantos1509@gmail.com>
2026-04-08 02:14:26 -0300

    chore: add DTO and Mapper class for 'NinjaModel' Entity

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/dto/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/dto/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.dto;
 
+import dev.java10x.CadastroDeNinjas.Missoes.model.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private String imgUrl;
+    private MissoesModel missoes;
+    private String rank;
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/mapper/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/mapper/NinjaMapper.java
@@ -1,4 +1,35 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.mapper;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.dto.NinjaDTO;
+import dev.java10x.CadastroDeNinjas.Ninjas.model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO dto){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(dto.getId());
+        ninjaModel.setNome(dto.getNome());
+        ninjaModel.setEmail(dto.getEmail());
+        ninjaModel.setRank(dto.getRank());
+        ninjaModel.setMissoes(dto.getMissoes());
+        ninjaModel.setImgUrl(dto.getImgUrl());
+        ninjaModel.setIdade(dto.getIdade());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+
+        return ninjaDTO;
+    }
 }
